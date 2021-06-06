efbb49eb2225b153b9bd7b92c58dae8fa5d6fde3
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-06 21:31:20 +0530

    Welcome Message

---
+++ LineComparision.java
@@ -0,0 +1,12 @@
+package linecomparision;
+
+public class LineComparision {
+	public static void lineCompare() {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+	public static void main(String[] args) {
+		LineComparision lineCompariosion = new LineComparision();
+		lineCompariosion.lineCompare();
+	}
+}
